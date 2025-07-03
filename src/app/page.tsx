6f3cbf3a43628c60620b78fb9bcfc160aba54f3a
Guilherme Chalita <guilhermehchalita@gmail.com>
2025-07-03 12:15:30 -0300

    feat: Add hover style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 import { FaGithub } from 'react-icons/fa'
 import { FcGoogle } from 'react-icons/fc'
 import { PiRocketLaunch } from 'react-icons/pi'
-import homeImage from '@/assets/home.png'
+import loginImage from '@/assets/login.png'
 
-export default function Home() {
+export default function Login() {
   return (
     <div className="flex h-screen max-w-[calc(100vw-(100vw-1196px)/2))] items-center gap-56">
       <div className="pl-5">
-        <Image src={homeImage} quality={100} priority alt="Home image" className="" />
+        <Image src={loginImage} quality={100} priority alt="Login image" className="" />
       </div>
 
       <div>
@@ -22,19 +22,19 @@ export default function Home() {
         <section className="mt-10 flex flex-col items-start gap-4">
           <button
             type="button"
-            className="flex w-full cursor-pointer items-center gap-5 rounded-lg bg-gray-600 py-5 pl-6 text-button-lg"
+            className="flex w-full cursor-pointer items-center gap-5 rounded-lg bg-gray-600 py-5 pl-6 text-button-lg transition hover:bg-gray-500"
           >
             <FcGoogle size={32} /> Entrar com Google
           </button>
           <button
             type="button"
-            className="flex w-full cursor-pointer items-center gap-5 rounded-lg bg-gray-600 py-5 pl-6 text-button-lg"
+            className="flex w-full cursor-pointer items-center gap-5 rounded-lg bg-gray-600 py-5 pl-6 text-button-lg transition hover:bg-gray-500"
           >
             <FaGithub size={32} /> Entrar com Github
           </button>
           <button
             type="button"
-            className="flex w-full cursor-pointer items-center gap-5 rounded-lg bg-gray-600 py-5 pl-6 text-button-lg"
+            className="flex w-full cursor-pointer items-center gap-5 rounded-lg bg-gray-600 py-5 pl-6 text-button-lg transition hover:bg-gray-500"
           >
             <PiRocketLaunch size={32} className="text-purple-100" /> Acessar como visitante
           </button>
