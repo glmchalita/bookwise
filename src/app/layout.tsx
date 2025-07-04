a2fe84fc3ad2ac6e6890ac12fa0119f3283ee3a2
Guilherme Chalita <guilhermehchalita@gmail.com>
2025-07-04 17:56:48 -0300

    feat: Implement react query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Nunito_Sans } from 'next/font/google'
 import './globals.css'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 
 const nunitoSans = Nunito_Sans({
   subsets: ['latin'],
@@ -18,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={`${nunitoSans.className} antialiased`}>
-      <body>{children}</body>
+      <body>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </body>
     </html>
   )
 }
