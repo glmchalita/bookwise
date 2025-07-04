dca5bdd7d8483e669b860185fc87ec2a447e5fbb
Guilherme Chalita <guilhermehchalita@gmail.com>
2025-07-04 11:13:50 -0300

    feat: Implement login button to aside layout

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import { FaArrowRightToBracket } from 'react-icons/fa6'
 import { Logo } from '@/assets/logo'
 import { Nav } from '../(components)/nav'
 
@@ -8,9 +10,20 @@ export default function RootLayout({
 }>) {
   return (
     <div className="grid min-h-screen grid-cols-[minmax(12rem,15rem)_1fr] gap-24">
-      <aside className="fixed mt-5 ml-5 flex h-[calc(100dvh-2.5rem)] flex-col items-center rounded-xl bg-gray-950 px-14 pt-10">
-        <Logo />
-        <Nav />
+      <aside className="fixed mt-5 ml-5 flex h-[calc(100dvh-2.5rem)] flex-col items-center justify-between rounded-xl bg-gray-950 px-14 pt-10">
+        <header>
+          <Logo />
+          <Nav />
+        </header>
+
+        <footer>
+          <Link
+            href="/"
+            className="mb-7 flex cursor-pointer items-center justify-center gap-3 rounded-sm px-2 py-1 text-button-md text-gray-200 hover:bg-gray-200/4"
+          >
+            Fazer login <FaArrowRightToBracket size={20} className="text-green-100" />
+          </Link>
+        </footer>
       </aside>
 
       <main className="col-start-2 max-w-screen bg-gray-950">{children}</main>
