ecadee59aabf0224db0b396b64ae326b7acd0ed8
Guilherme Chalita <guilhermehchalita@gmail.com>
2025-07-04 10:53:42 -0300

    refactor: Update font to Nunito Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
+import { Nunito_Sans } from 'next/font/google'
 import './globals.css'
 
-const nunito = Nunito({
+const nunito = Nunito_Sans({
   subsets: ['latin'],
 })
 
