b6e8147f946fbe3fbea852d1b13244602a5a443d
Guilherme Chalita <guilhermehchalita@gmail.com>
2025-07-11 00:08:45 -0300

    feat: Add hover card to timestamp component

--- src/app/(components)/timestamp.tsx
+++ src/app/(components)/timestamp.tsx
@@ -1,3 +1,9 @@
+import {
+  HoverCard,
+  HoverCardArrow,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@radix-ui/react-hover-card'
 import dayjs from '@/lib/dayjs'
 
 interface TimestampProps {
@@ -6,5 +12,21 @@ interface TimestampProps {
 }
 
 export function Timestamp({ date, className }: TimestampProps) {
-  return <span className={`text-body-sm capitalize ${className}`}>{dayjs(date).fromNow()}</span>
+  return (
+    <HoverCard>
+      <HoverCardTrigger className="w-fit">
+        <span className={`text-body-sm capitalize ${className} cursor-default`}>
+          {dayjs(date).fromNow()}
+        </span>
+      </HoverCardTrigger>
+      <HoverCardContent
+        className="rounded-md bg-gray-600/60 px-1 py-0.5 text-body-xs text-gray-300"
+        align="center"
+        side="bottom"
+      >
+        {dayjs(date).format('D [de] MMMM [de] YYYY [às] H[:]MM')}
+        <HoverCardArrow className="fill-gray-600/60" />
+      </HoverCardContent>
+    </HoverCard>
+  )
 }
