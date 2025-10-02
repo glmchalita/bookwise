b32ccb34ddaa0c3e7caa9eca29fbcc23ef7fe746
Guilherme Chalita <guilhermehchalita@gmail.com>
2025-10-02 02:51:15 -0300

    chore: Clean up blank spaces

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,12 @@
 'use client'
 
-import { QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from 'next-auth/react'
 import type { ReactNode } from 'react'
-import { queryClient } from '@/lib/react-query'
 
 interface ProvidersProps {
   children: ReactNode
 }
 
 export default function Providers({ children }: ProvidersProps) {
-  return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </SessionProvider>
-  )
+  return <SessionProvider>{children}</SessionProvider>
 }
